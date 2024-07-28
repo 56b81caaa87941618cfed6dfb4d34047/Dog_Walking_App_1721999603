8dbeaa1a724a74940762f58a46f977a986d59fb6
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-28 15:33:19 +0000

    Code generation

--- components/purple_header_component_1721999604.js
+++ components/purple_header_component_1721999604.js
@@ -1,25 +1,25 @@
 Vue.component("purple_header_component_1721999604", {
     template: `
-    <header class="bg-gradient-to-r from-blue-500 to-indigo-600 py-4 px-6 shadow-lg">
+    <header class="bg-gradient-to-r from-purple-900 to-indigo-900 py-6 px-8 shadow-2xl">
         <div class="max-w-7xl mx-auto flex items-center justify-between">
             <div class="flex items-center">
                 <a href="index.html" class="flex items-center">
-                    <img src="./images/logo.svg" alt="Logo" class="h-8 w-8 mr-2">
-                    <span class="text-white text-xl font-bold">MyApp</span>
+                    <img src="./images/logo.svg" alt="Logo" class="h-10 w-10 mr-3">
+                    <span class="text-purple-200 text-2xl font-bold tracking-wider">MyApp</span>
                 </a>
             </div>
-            <nav class="flex items-center space-x-6">
-                <a href="#" class="text-white hover:text-blue-200 transition duration-300">
-                    <i class='bx bx-home text-2xl'></i>
+            <nav class="flex items-center space-x-8">
+                <a href="#" class="text-purple-300 hover:text-purple-100 transition duration-300">
+                    <i class='bx bx-home text-3xl'></i>
                 </a>
-                <a href="#" class="text-white hover:text-blue-200 transition duration-300">
-                    <i class='bx bx-search text-2xl'></i>
+                <a href="#" class="text-purple-300 hover:text-purple-100 transition duration-300">
+                    <i class='bx bx-search text-3xl'></i>
                 </a>
-                <a href="#" class="text-white hover:text-blue-200 transition duration-300">
-                    <i class='bx bx-bell text-2xl'></i>
+                <a href="#" class="text-purple-300 hover:text-purple-100 transition duration-300">
+                    <i class='bx bx-bell text-3xl'></i>
                 </a>
-                <a href="#" class="text-white hover:text-blue-200 transition duration-300">
-                    <i class='bx bx-user text-2xl'></i>
+                <a href="#" class="text-purple-300 hover:text-purple-100 transition duration-300">
+                    <i class='bx bx-user text-3xl'></i>
                 </a>
             </nav>
         </div>
