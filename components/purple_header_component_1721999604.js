a7c1fea8568b783b32b31a62464a14ba7b51752a
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-28 15:33:07 +0000

    Code generation

--- components/purple_header_component_1721999604.js
+++ components/purple_header_component_1721999604.js
@@ -1,57 +1,28 @@
-/* Summary: HTML file that describes the header section including:
-1. Site branding with a logo on the left.
-2. Desktop navigation menu with links to different pages in the middle.
-3. Desktop sign-in and sign-up links on the right.
-*/ 
-
 Vue.component("purple_header_component_1721999604", {
     template: `
-    <header class="bg-gradient-to-r from-purple-900 to-blue-900 py-6 px-8 shadow-lg">
+    <header class="bg-gradient-to-r from-blue-500 to-indigo-600 py-4 px-6 shadow-lg">
         <div class="max-w-7xl mx-auto flex items-center justify-between">
             <div class="flex items-center">
                 <a href="index.html" class="flex items-center">
-                    <img src="./images/logo.svg" alt="Stellar" class="h-10 w-10 mr-3">
-                    <span class="text-purple-400 text-3xl font-bold">Stellar</span>
+                    <img src="./images/logo.svg" alt="Logo" class="h-8 w-8 mr-2">
+                    <span class="text-white text-xl font-bold">MyApp</span>
                 </a>
-                <nav class="hidden md:flex ml-12">
-                    <a href="Book Walks.html" class="text-white hover:text-purple-300 mx-5 text-lg">Book Walks</a>
-                    <a href="Walker Profiles.html" class="text-white hover:text-purple-300 mx-5 text-lg">Walker Profiles</a>
-                    <a href="Doggy Day Care.html" class="text-white hover:text-purple-300 mx-5 text-lg">Doggy Day Care</a>
-                    <a href="Pet Supplies.html" class="text-white hover:text-purple-300 mx-5 text-lg">Pet Supplies</a>
-                    <a href="My Account.html" class="text-white hover:text-purple-300 mx-5 text-lg">My Account</a>
-                </nav>
             </div>
-            <div class="flex items-center">
-                <button @click="openMyAccount" class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-3 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
-                    My Account
-                </button>
-                <button @click="expanded = !expanded" class="ml-6 text-white md:hidden">
-                    <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                    </svg>
-                </button>
-            </div>
+            <nav class="flex items-center space-x-6">
+                <a href="#" class="text-white hover:text-blue-200 transition duration-300">
+                    <i class='bx bx-home text-2xl'></i>
+                </a>
+                <a href="#" class="text-white hover:text-blue-200 transition duration-300">
+                    <i class='bx bx-search text-2xl'></i>
+                </a>
+                <a href="#" class="text-white hover:text-blue-200 transition duration-300">
+                    <i class='bx bx-bell text-2xl'></i>
+                </a>
+                <a href="#" class="text-white hover:text-blue-200 transition duration-300">
+                    <i class='bx bx-user text-2xl'></i>
+                </a>
+            </nav>
         </div>
-        <nav x-show="expanded" class="md:hidden mt-6">
-            <a href="Book Walks.html" class="block text-white hover:text-purple-300 py-3 text-lg">Book Walks</a>
-            <a href="Walker Profiles.html" class="block text-white hover:text-purple-300 py-3 text-lg">Walker Profiles</a>
-            <a href="Doggy Day Care.html" class="block text-white hover:text-purple-300 py-3 text-lg">Doggy Day Care</a>
-            <a href="Pet Supplies.html" class="block text-white hover:text-purple-300 py-3 text-lg">Pet Supplies</a>
-            <a href="My Account.html" class="block text-white hover:text-purple-300 py-3 text-lg">My Account</a>
-        </nav>
     </header>
-    data() {
-        return {
-            expanded: false,
-        };
-    },
-        methods: {
-            // Start of openMyAccount method
-            openMyAccount() {
-                // Add logic to open My Account section
-                console.log('Opening My Account section');
-            }
-            // End of openMyAccount method
-        },
-    });
-                    
+    `,
+});
